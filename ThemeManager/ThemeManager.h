9e227d87abc671fe0091f4b8a676e38f3d60ce66
Yozone Wang <wangyaozh@gmail.com>
2019-06-15 00:47:01 +0800

    Initial Commit

---
+++ ThemeManager/ThemeManager.h
@@ -0,0 +1,19 @@
+//
+//  ThemeManager.h
+//  ThemeManager
+//
+//  Created by 王要正 on 2019/6/15.
+//  Copyright © 2019 王要正. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ThemeManager.
+FOUNDATION_EXPORT double ThemeManagerVersionNumber;
+
+//! Project version string for ThemeManager.
+FOUNDATION_EXPORT const unsigned char ThemeManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThemeManager/PublicHeader.h>
+
+
